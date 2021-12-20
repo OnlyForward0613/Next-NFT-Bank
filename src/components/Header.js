550848e3ffa86a47d784a87e1a112711d42a4711
OnlyForward0613 <harryborison613@gmail.com>
2021-12-21 08:21:45 +0900

    dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+import Link from 'next/link'
 import { ConnectButton } from "./styleHook";
 
 export default function Header({
@@ -11,12 +13,28 @@ export default function Header({
       <div className="logo">
         NFT Bank
       </div>
-      <ConnectButton onClick={connectWallet} disabled={connected}>
-        {connected ?
-          signerAddress.slice(0, 4) + "..." + signerAddress.slice(39, 42) :
-          "Connect"
-        }
-      </ConnectButton>
+      <div className="nav">
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/bank">
+              <a>BANK</a>
+            </Link>
+          </li>
+          <li>
+            <ConnectButton onClick={connectWallet} disabled={connected}>
+              {connected ?
+                signerAddress.slice(0, 4) + "..." + signerAddress.slice(39, 42) :
+                "Connect"
+              }
+            </ConnectButton>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
---
+++ src/pages/bank.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Bank() {
+  return (
+    <>
+      <Head>
+        <title>NFT Bank | Bank</title>
+        <meta name="description" content="NFT Bank" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
