d4efa98a4a356e33404621cc723aa4d551cf8f9d
OnlyForward0613 <harryborison613@gmail.com>
2021-12-21 11:14:43 +0900

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import { Button } from "@mui/material";
+import { useState } from "react"
 import Link from 'next/link'
-import { ConnectButton } from "./styleHook";
+import { ConnectButton } from "./styleHook"
 
 export default function Header({
   signerAddress,
@@ -8,6 +8,9 @@ export default function Header({
   connected,
   ...props
 }) {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <header>
       <div className="logo">
@@ -43,6 +46,61 @@ export default function Header({
             </ConnectButton>
           </li>
         </ul>
+      </div>
+      <div className="mobile-nav">
+        <div className="mobile-action">
+          <ConnectButton onClick={connectWallet} disabled={connected} style={{ marginRight: 50 }}>
+            {connected ?
+              signerAddress.slice(0, 4) + "..." + signerAddress.slice(39, 42) :
+              "Connect"
+            }
+          </ConnectButton>
+          <div className="hamburger" style={{ position: !open ? "absolute" : "fixed" }}>
+            <button onClick={() => setOpen(!open)} className="ham">
+              {!open ?
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="1" y="2" width="16" height="2" rx="1" fill="#fff" />
+                  <rect x="1" y="8" width="16" height="2" rx="1" fill="#fff" />
+                  <rect x="1" y="14" width="16" height="2" rx="1" fill="#fff" />
+                </svg>
+                :
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clipPath="url(#clip0_7_12)">
+                    <path d="M15.5668 1.42462L9.20279 7.78859L2.83883 1.42463C2.13173 0.717514 0.717513 2.13173 1.42462 2.83884L7.78858 9.2028L1.42462 15.5668C0.717514 16.2739 2.13173 17.6881 2.83883 16.981L9.20279 10.617L15.5668 16.981C16.2739 17.6881 17.6881 16.2739 16.981 15.5668L10.617 9.2028L16.981 2.83884C17.6881 2.13173 16.2739 0.717515 15.5668 1.42462Z" fill="#fff" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_7_12">
+                      <rect width="18" height="18" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              }
+            </button>
+          </div>
+        </div>
+        <div className={!open ? "mobile-dropdown" : "mobile-dropdown active"}>
+          <ul>
+            <li>
+              <Link href="/">
+                <a onClick={() => setOpen(false)}>Home</a>
+              </Link>
+            </li>
+            {connected &&
+              <>
+                <li>
+                  <Link href="/mint">
+                    <a onClick={() => setOpen(false)}>MINT</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/bank">
+                    <a onClick={() => setOpen(false)}>BANK</a>
+                  </Link>
+                </li>
+              </>
+            }
+          </ul>
+        </div>
       </div>
     </header >
   )
