79d334b1a71f9694803e0936bc073f24b0d0d86c
OnlyForward0613 <harryborison613@gmail.com>
2022-01-13 19:35:33 +0900

    add telegram button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -91,6 +91,28 @@ export default function Sidebar({ connected, ...props }) {
               twitter
             </span>
           </a>
+          <a
+            href="https://t.me/DustyVaults"
+            target="_blank"
+            rel="noreferrer"
+            className="social-link discord"
+          >
+            <div className="social-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_45_10)">
+                  <path d="M22.0517 2.12925L0.788779 10.3711C-0.0668777 10.7549 -0.356284 11.5235 0.581966 11.9407L6.03681 13.6831L19.2259 5.48986C19.9461 4.9755 20.6833 5.11266 20.0489 5.67849L8.72125 15.9879L8.36542 20.3509C8.695 21.0245 9.29847 21.0277 9.6834 20.6928L12.8174 17.7121L18.1848 21.7521C19.4314 22.494 20.1098 22.0152 20.378 20.6555L23.8985 3.89911C24.2641 2.22544 23.6407 1.488 22.0517 2.12925V2.12925Z" fill="#fff" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_45_10">
+                    <rect width="24" height="24" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+            <span>
+              telegram
+            </span>
+          </a>
           <div>
             <h5>$Dusty</h5>
             <p>
